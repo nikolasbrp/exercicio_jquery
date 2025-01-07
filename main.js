c60173cbc228c416944daddcff06328d8533ab39
nikolasbrp <npovreslo@gmail.com>
2025-01-07 16:30:44 -0300

    :tada: Commit inicial

---
+++ main.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    $('#btn-add-tarefa').click(function(e) {
+        e.preventDefault();
+
+        const nomeDaTarefa = $('#nome-tarefa').val();        
+
+        const novoItem = $('<li></li>');
+        $(`<p>&#x2022; ${nomeDaTarefa}</p>`).appendTo(novoItem);        
+        $(novoItem).appendTo('main ul');
+        $('#nome-tarefa').val(''); 
+    });
+
+    $('ul').on('click', 'li', function() {
+        $(this).toggleClass('texto-riscado');
+    });
+});
